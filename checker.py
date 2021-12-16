ebb0f8a0c6d274237f069ef092997c3d529007d2
RandomBlbosti <73714335+RandomBlbosti@users.noreply.github.com>
2021-12-16 18:56:22 +0100

    Add files via upload

--- checker.py
+++ checker.py
@@ -4,23 +4,17 @@ from pyfiglet import figlet_format
 import ctypes
 import re
 import requests
-
 init(convert=True)
-
 ctypes.windll.kernel32.SetConsoleTitleW("GD Username Checker")
 art = figlet_format('GD Username Checker')
-print(art)
 author = figlet_format('by RandomB#6160')
-print(author)
+print(art, author)
 invalid = 0
 valid = 0
 clear = lambda: os.system('cls')
-print("Please type the name of the .txt file with the names: ")
-nam = str(input(''))
-print("Please type the name of the .txt file to save the valid names: ")
-savenam = str(input(''))
-name = nam.replace('.txt', '')
-savename = savenam.replace('.txt', '')
+name = str(input("Please type the name of the .txt file with the names: ")).replace(".txt", "")
+savename = str(input("Please type the name of the .txt file to save the valid names: ")).replace(".txt", "")
+turnon = str(input("Print usernames into the console? (Y/N)\n")).upper()
 file1 = open(name + '.txt', 'r')
 Lines = file1.readlines()
 clear()
@@ -39,19 +33,20 @@ for line in Lines:
     if data == "-1":
         if re.match("^[A-Za-z0-9_-]*$", playername):
             valid += 1
-            #print(f"{Fore.GREEN}[+] {playername}")
+            if turnon == "Y":
+                print(f"{Fore.GREEN}[+] {playername}")
             ctypes.windll.kernel32.SetConsoleTitleW(f"GD Username Checker | Valid: {valid} | Invalid: {invalid}")
             k = open(f'{savename}.txt', 'a')
             k.write(f"{playername}\n")
             k.close()
         else:
             invalid += 1
-            #print(f"{Fore.RED}[-] {playername} (ILLEGAL CHARACTER)")
+            if turnon == "Y":
+                print(f"{Fore.RED}[-] {playername} (ILLEGAL CHARACTER)")
             ctypes.windll.kernel32.SetConsoleTitleW(f"GD Username Checker | Valid: {valid} | Invalid: {invalid}")
-
     else:
         invalid += 1
-        #print(f"{Fore.RED}[-] {playername}")
+        if turnon == "Y":
+            print(f"{Fore.RED}[-] {playername}")
         ctypes.windll.kernel32.SetConsoleTitleW(f"GD Username Checker | Valid: {valid} | Invalid: {invalid}")
-print(f"\n\n{Fore.RESET}Thanks for using my program. \nAll valid codes have been saved to {savename}.txt. \nSee you later. :)")
-input("")
+input(f"\n\n{Fore.RESET}Thanks for using my program. \nAll valid codes have been saved to {savename}.txt. \nSee you later. :)\n")
