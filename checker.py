6def54a6da62a01317c22625b542724c0ee2ec94
RandomBlbosti <73714335+RandomBlbosti@users.noreply.github.com>
2022-01-09 18:01:45 +0100

    v2.1
    v2.1

--- checker.py
+++ checker.py
@@ -19,7 +19,11 @@ file1 = open(name + '.txt', 'r')
 Lines = file1.readlines()
 clear()
 for line in Lines:
+    numbered = False
     playername = line.replace('\n', '')
+    if playername.isnumeric() and not playername.endswith(" "):
+        numbered = True
+        playername += " "
     url = "http://www.boomlings.com/database/getGJUsers20.php"
     p = f"gameVersion=21&binaryVersion=35&gdw=0&str={playername}&total=0&page=0&secret=Wmfd2893gb7"
     p = p.encode()
@@ -31,7 +35,7 @@ for line in Lines:
     }
     data = requests.post(url, p, headers=headers).text
     if data == "-1":
-        if re.match("^[A-Za-z0-9_-]*$", playername):
+        if re.match("^[A-Za-z0-9_-]*$", playername) or numbered:
             valid += 1
             if turnon == "Y":
                 print(f"{Fore.GREEN}[+] {playername}")
